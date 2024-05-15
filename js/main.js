012dffe97bef54bdff55e4eacd84424f593fb280
Sergeyfdf <dimka5355k@gmail.com>
2024-05-15 17:07:58 +0300

    header__adaptive+

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function(){
+  document.querySelector('.menu__btn').addEventListener('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
     var mixer = mixitup(".portfolio__content");
 
     $('.slider__inner').slick({
@@ -6,9 +9,17 @@ $(function(){
         infinite: true,
         speed: 300,
         prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow"></button>'
+        nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow"></button>',
       });
-})
+
+      // $('.menu__btn').on('click', function(){
+      //   $('.header__top-inner').toggleClass('header__top-inner--active');
+      // });
+
+      
+                                                                 
+      var mixer = mixitup(".portfolio__content");
+});
 
 
   
